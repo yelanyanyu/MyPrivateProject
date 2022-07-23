3dcfc6fc761c47b989489d5e01dad7c210bb2d88
yelanyanyu <2831142384@qq.com>
2022-07-23 22:43:38 +0800

    项目初始化

---
+++ 满汉楼/project/src/com/dao/BasicDAO.java
@@ -0,0 +1,69 @@
+package com.dao;
+
+import com.utils.JDBCUtils_Druid;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * @author yelanyanyu@zjxu.edu.cn
+ * @version 1.0
+ */
+public class BasicDAO<T> {
+    private QueryRunner queryRunner = new QueryRunner();
+
+    public int update(String sql, Object... param) {
+        Connection connection = null;
+        try {
+            connection = JDBCUtils_Druid.getConnection();
+            return queryRunner.update(connection, sql, param);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            JDBCUtils_Druid.closeAll(null, null, connection);
+        }
+    }
+
+    public List<T> QueryMany(String sql, Class<T> clazz, Object... param) {
+        Connection connection = null;
+        try {
+            connection = JDBCUtils_Druid.getConnection();
+            return queryRunner.query(connection, sql, new BeanListHandler<T>(clazz), param);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            JDBCUtils_Druid.closeAll(null, null, connection);
+        }
+    }
+
+    public T QuerySingleLine(String sql, Class<T> clazz, Object... param) {
+        Connection connection = null;
+        try {
+            connection = JDBCUtils_Druid.getConnection();
+            T query = queryRunner.query(connection, sql, new BeanHandler<>(clazz), param);
+            return queryRunner.query(connection, sql, new BeanHandler<>(clazz), param);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            JDBCUtils_Druid.closeAll(null, null, connection);
+        }
+    }
+
+    public Object QueryScalar(String sql, Class<T> clazz, Object... param) {
+        Connection connection = null;
+        try {
+            connection = JDBCUtils_Druid.getConnection();
+            return queryRunner.query(connection, sql, new ScalarHandler(), param);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            JDBCUtils_Druid.closeAll(null, null, connection);
+        }
+    }
+
+}
---
+++ 满汉楼/project/src/com/utils/JDBCUtils_Druid.java
@@ -0,0 +1,56 @@
+package com.utils;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+/**
+ * @author yelanyanyu@zjxu.edu.cn
+ * @version 1.0
+ */
+@SuppressWarnings({"all"})
+public class JDBCUtils_Druid {
+    public static DataSource dataSource;
+
+    static {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream("src\\druid.properties"));
+            dataSource = DruidDataSourceFactory.createDataSource(properties);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static boolean closeAll(ResultSet resultSet, Statement statement, Connection connection) {
+        boolean flag = false;
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+            return (flag = true);
+        } catch (SQLException throwables) {
+            throw new RuntimeException(throwables);
+        }
+    }
+}
