be1eb659ceba4b77058229caf7b1c7c98926cde7
yelanyanyu <2831142384@qq.com>
2022-07-23 22:59:20 +0800

    主界面初始化完毕

--- 满汉楼/project/src/com/view/MHLview.java
+++ 满汉楼/project/src/com/view/MHLview.java
@@ -1,11 +1,33 @@
 package com.view;
 
+import com.utils.Utility;
+
 /**
  * @author yelanyanyu@zjxu.edu.cn
  * @version 1.0
  */
 public class MHLview {
-    public void mainmenu(){
+    //显示主菜单
+    public void mainmenu() {
+        while (true) {
+            System.out.println("======满汉楼======");
+            System.out.println("\t\t1.登录满汉楼;");
+            System.out.println("\t\t2.退出满汉楼;");
+            System.out.println("请输入你的选择:");
+            int i = Utility.readInt();
+            if (i == 1) {
+                secondMenu();
+                break;
+            } else if (i == 2) {
+                System.out.println("退出成功~");
+                break;
+            } else {
+                System.out.println("请输入正确的选择");
+            }
+        }
+    }
+
+    public void secondMenu() {
 
     }
 }
---
+++ 满汉楼/project/src/com/view/Test.java
@@ -0,0 +1,10 @@
+package com.view;
+/**
+ * @author yelanyanyu@zjxu.edu.cn
+ * @version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        new MHLview().mainmenu();
+    }
+}
